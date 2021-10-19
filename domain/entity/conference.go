60d8f000ebc687d1b3d2d0a6bedbd5595c0ba560
a.muradyan <a.muradyan@corp.mail.ru>
2021-10-19 21:29:45 +0300

    after merge

--- domain/entity/conference.go
+++ domain/entity/conference.go
@@ -7,13 +7,7 @@ type Conference struct {
 	Participants []*User `json:"participants"`
 	HostUserID   string  `json:"host_user_id"`
 	BridgeID     string  `json:"-"`
-<<<<<<< HEAD
-<<<<<<< HEAD
 	IsRecording  bool    `json:"is_recording"`
-=======
->>>>>>> 977da2b (rebase inbloud)
-=======
->>>>>>> 9dff4d40660aa86a5ea66aa72ef8bfb947260101
 }
 
 func (c Conference) Less(then btree.Item) bool {
