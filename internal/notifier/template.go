f322f8977abffc061abdb431cc26c77869a6680b
a.muradyan <a.muradyan@corp.mail.ru>
2021-11-24 05:32:14 +0300

    fix inline

--- internal/notifier/template.go
+++ internal/notifier/template.go
@@ -18,55 +18,13 @@ const (
 		<title>Protocall</title>
 		<meta charset="utf-8">
 	</head>
-	<style>
-		* {
-			margin: 0;
-			padding: 0;
-			box-sizing: border-box;
-			}
-
-		body {
-			margin: 0;
-			font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
-			-webkit-font-smoothing: antialiased;
-			-moz-osx-font-smoothing: grayscale;
-			background: #282c34; 
-			font-size: 16px;
-		}
-
-		body h1 {
-			font-size: 21px;
-			color: gainsboro;
-			margin: 10px;
-		}
-
-		.message {
-			background: #3f4148;
-			padding: 10px;
-			margin: 5px;
-			border-radius: 10px;
-			color: rgb(223, 223, 223);
-		}
-
-		.message_user-container {
-			font-weight: 700;
-			width: 100%;
-			margin-bottom: 5px;
-			display: flex;
-			justify-content: space-between;
-		}
-
-		.message_time {
-			font-weight: 200;
-		}
-	</style>
-	<body>
-		<h1>Протокол вашей конференции от {{(index .Phrases 0).Time.Format "02.01"}}.</h1>
+	<body style="margin: 0; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif; -webkit-font-smoothing: antialiased; -moz-osx-font-smoothing: grayscale; background: #282c34; font-size: 16px;">
+		<h1 style="font-size: 21px; color: gainsboro; margin: 10px;">Протокол вашей конференции от {{(index .Phrases 0).Time.Format "02.01"}}.</h1>
 		{{range $phrase := .Phrases}}
-			<div class="message">
-				<div class="message_user-container">
+			<div class="message" style="background: #3f4148; padding: 10px; margin: 5px; border-radius: 10px; color: rgb(223, 223, 223);">
+				<div class="message_user-container" style="font-weight: 700; width: 100%; margin-bottom: 5px; display: flex; justify-content: space-between;">
 					<span class="message_user">{{$phrase.User}}</span>
-					<span class="message_time">{{$phrase.Time.Format "15:04"}}</span>
+					<span class="message_time" style="font-weight: 200;">{{$phrase.Time.Format "15:04"}}</span>
 				</div>
 				<p class="message_text">{{$phrase.Text}}</p>
 			</div>
