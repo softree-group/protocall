c75e012278b38fe930e5b91fa309217cf93230a2
Mark <m.biem@outlook.com>
2021-12-11 19:21:16 +0300

    paddings

--- internal/notifier/template.go
+++ internal/notifier/template.go
@@ -23,12 +23,12 @@ const (
 	<body style="margin: 0; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif; -webkit-font-smoothing: antialiased; -moz-osx-font-smoothing: grayscale; background: #282c34; font-size: 16px;">
 		<h1 style="font-size: 21px; color: gainsboro; margin: 10px;">Протокол вашей конференции от {{(index .Phrases 0).Time.Format "02.01"}}.</h1>
 		{{range $phrase := .Phrases}}
-			<div class="message" style="background: #3f4148; padding: 10px; margin: 5px; border-radius: 10px; color: rgb(223, 223, 223);">
+			<div class="message" style="background: #3f4148; padding: 0 10px; margin: 5px; border-radius: 10px; color: rgb(223, 223, 223); height: fit-content; padding-top: 5px;">
 				<div class="message_user-container" style="position: relative; font-weight: 700; width: 100%; margin-bottom: 5px;">
 					<span class="message_user">{{$phrase.User}}</span>
 					<span class="message_time" style="position: absolute; right: 0; font-weight: 200;">{{$phrase.Time.Format "15:04"}}</span>
 				</div>
-				<p class="message_text">{{$phrase.Text}}</p>
+				<p class="message_text" style="margin: 5px 0;">{{$phrase.Text}}</p>
 			</div>
 		{{end}}
 	</body>
